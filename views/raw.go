a4e72c57d9f8ee757881dc87b76031e9bc655996
Davidson <Joe.Davidson@ipfin.co.uk>
2021-05-27 10:15:34 +0100

    Escape string

--- views/raw.go
+++ views/raw.go
@@ -19,7 +19,7 @@ func RawTemplate(c templ.Component) (t templ.Component) {
 	return templ.ComponentFunc(func(ctx context.Context, w io.Writer) (err error) {
 		var buf bytes.Buffer
 		c.Render(ctx, &buf)
-		_, err = io.WriteString(w, buf.String())
+		_, err = io.WriteString(w, templ.EscapeString(buf.String()))
 		return err
 	})
 }
